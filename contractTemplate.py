9a84807d72224476ccdb32efd459ee5f0b4cb43e
Don Cloud <51292470+xiaochuan8264@users.noreply.github.com>
2023-12-18 23:16:14 +0800

    读取html，并填充模板合同

--- contractTemplate.py
+++ contractTemplate.py
@@ -1,4 +1,5 @@
 from docxtpl import DocxTemplate
+from bs4 import BeautifulSoup as bf
 
 # word模板中，所需字段必须以{{字段内容}}形式展示，否则无法成功替换
 
@@ -22,3 +23,30 @@ context = {"year":"2023",
 doc.render(context)
 
 doc.save("filled_document.docx")
+
+# 字段名称
+htmlfile = "filelocation"
+
+with open(file,'r',encoding='utf-8') as f:
+    soup = bf(f.read(),'lxml')
+
+# 找到指定的fieldname
+element = soup.find(attrs={"data-fieldname": "lxr"})
+def has_data_fieldname(tag):
+    return tag.has_attr('data-fieldname')
+           
+nodes_with_data_fieldname = soup.find_all(has_data_fieldname)
+
+data-fieldnames={"gysmc" : "供应商名称",
+                 "frsfzh": "身份证号",
+                 "dz": "联系地址",
+                 "lxr": "联系人",
+                 "lxsj": "联系电话",
+                 "lxyx": "联系邮箱",
+                 "qq": "联系QQ",
+                 "khxdz": "开户行地址",
+                 "zhmc":"账户名称",
+                 "yxzh":"银行账户"}
+
+child_with_value = parent_div.find(attrs={'value': True})
+value = child_with_value['value'] if child_with_value else None
